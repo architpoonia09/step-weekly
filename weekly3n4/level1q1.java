aedf9b0f94ef869148317cc016fee13b98cdbc1c
architpoonia09 <architpoonia09@gmail.com>
2026-03-11 08:54:00 +0530

    level1q2

--- weekly3n4/level1q1.java
+++ weekly3n4/level1q1.java
@@ -1,13 +1,16 @@
 package weekly3n4;
-
+import java.util.Scanner;
 public class level1q1 {
     public static void main(String[] args) {
-        int num = 10;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
         if (num % 5 == 0) {
             System.out.println(num + " is divisible by 5.");
         } else {
             System.out.println(num + " is not divisible by 5.");
         }
+        scanner.close();
     }
     
 }
