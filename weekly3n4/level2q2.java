3cfd7db82615eff639656378e3832038737b2292
architpoonia09 <architpoonia09@gmail.com>
2026-03-31 15:45:00 +0530

    level2q2

---
+++ weekly3n4/level2q2.java
@@ -0,0 +1,16 @@
+package weekly3n4;
+import java.util.Scanner;
+public class level2q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+        scanner.close();
+        System.out.println("Multiplication table of " + number + "from 6 to 9:");
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+
+}
+}
